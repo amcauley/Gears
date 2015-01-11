32984567d9aa58c483db5e468e8a1efd41f28e52
amcauley <amcauley@ucla.edu>
2015-01-10 18:22:40 -0800

    Gear Tooth Profile as Input
    Now the input profile of a single tooth is specified and the resulting
    cut gear is the output.

--- gearDev.m
+++ gearDev.m
@@ -1,62 +1,54 @@
-w = 2; %toothed gear rotation speed, rad/s. Cut gear is assumed to be 1 rad/s.
-r = 6; %toothed gear radius
+w = 1; %toothed gear rotation speed, rad/s. Cut gear is assumed to be 1 rad/s.
+rOut = 3; %toothed gear outer radius
+rIn = rOut*.76; %toothed gear inner radius
 theta0 = pi; %starting angle, toothed gear
 
-a = 10; %cut gear radius
+a = 3; %cut gear radius
 phi0 = 0; %starting angle, cut gear
 
-d = 15; %separation of gears (center to center)
+d = a+rIn; %separation of gears (center to center)
 
-Nt = 4096; %number of time samples
-Np = 3; %number of angular points
-
-phQ = .01; %angle quantization size in radians
+Nt = 128; %number of time samples
+Np = 18; %number of angular points per replication unit of toothed gear
 
 t = linspace(-pi*1.5,pi*1.5,Nt)';
 
+%INPUT GEAR DEFINITION (FOR A SINGLE TOOTH)
+nTeeth = 10;
+maxAngIn = 2*pi/nTeeth;
+angIn = linspace(0, maxAngIn, Np);
+radIn = ones(1,Np)*rIn;
+idx = angIn <= maxAngIn*1/6;
+radIn(idx) = rIn + (rOut-rIn)*angIn(idx)/(maxAngIn/6);
+idx = (angIn > maxAngIn*1/6) & (angIn <= maxAngIn*3/6);
+radIn(idx) = rOut;
+idx = (angIn > maxAngIn*3/6) & (angIn <= maxAngIn*4/6);
+radIn(idx) = rIn + (rOut-rIn)*(maxAngIn*4/6-angIn(idx))/(maxAngIn/6);
+
+%plot(radIn)
+%return
+
+%replicate input profile around the entire 2*pi radian gear
+angIn = linspace(0, 2*pi, Np*nTeeth);
+radIn = repmat(radIn, 1, nTeeth);
+
+
 %Create time/phase matrix, g. Up/Down within a column moves in time, left/right
 %within a row changes the phase of the point we're considering on the toothed gear.
-g = linspace(t, t+2*pi, Np+1)(:,1:end-1);
+g = linspace(t, t+2*pi, Np*nTeeth);
 g = pi - theta0 - w*g;
 
-ang = atan(r*sin(g)./(d-r*cos(g))) + phi0 + t*ones(1,Np);
+r = ones(Nt,1)*radIn;
+ang = atan(r.*sin(g)./(d-r.*cos(g))) + phi0 + t*ones(1,Np*nTeeth);
 mag = zeros(size(ang));
-mag = sqrt((d - r*cos(g)).^2 + r^2*sin(g).^2);
+mag = sqrt((d - r.*cos(g)).^2 + r.^2.*sin(g).^2);
 mag(mag > a) = a;
-
-ang = fix(ang/phQ)*phQ;
-minAng = min(min(ang));
-maxAng = max(max(ang));
-ang2 = minAng:phQ:maxAng;
-mag2 = zeros(size(ang2));
-for n = 1:length(ang2)
-    thisAng = ang2(n);
-    angColCnt = 0;
-    for i = 1:size(ang)(2)
-        if(sum(ang(:,i) == thisAng))
-            angColCnt = angColCnt + 1;
-        end
-    end
-    if(angColCnt == size(ang)(2))
-        mags = mag(ang == thisAng);
-        mag2(n) = min(mags);
-    end
-end
-
-ang2 = ang2(mag2 > 0);
-mag2 = mag2(mag2 > 0);
 
 %plotting functions treat each column as a separate function
 figure(1);
-subplot(2,1,1);
-plot(ang, mag);
-%hold on
-%scatter(ang2, mag2);
-%hold off;
-axis([minAng maxAng 0 a]);
-subplot(2,2,3);
-polar(ang, mag);
-subplot(2,2,4);
-polar(ang2, mag2);
+subplot(1,2,1);
+polar(angIn, radIn, 'k');
+subplot(1,2,2);
+polar(ang, mag, 'k');
 figure(2); %figure(2) used for debugging
 subplot(1,1,1);
